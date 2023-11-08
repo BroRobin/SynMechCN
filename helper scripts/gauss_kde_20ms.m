39fd0b6f3a2ea1f5e3aa6a2fa843c8e87242964b
BroRobin <146177334+BroRobin@users.noreply.github.com>
2023-11-08 09:21:13 +0100

    Add files via upload

---
+++ helper scripts/gauss_kde_20ms.m
@@ -0,0 +1,38 @@
+function [ff,testTimeVector] = gauss_kde_20ms(trials,timeVec)
+% input:
+% trails: cell array of spike times in different trials
+% time vec: relevant interval in which rates are extracted. 
+% output:
+% ff: spikefrequency signal
+% testTimeVector:
+
+% Nico Flierman, 2018
+
+collapsed_spikes = vertcat(trials{:,:});% 
+numTrials = length(trials);% sweeps 
+
+  
+ binSize = 0.020; % gaussian kernel size in seconds 
+
+ % take 1 ms steps if only begin and end are specified
+if length(timeVec)==2
+ testDt = 0.001; %1ms resolution
+ testTimeVector = timeVec(1):testDt:timeVec(end);
+else % otherwise use input steps 
+    testTimeVector = timeVec;
+end
+
+
+ %ksdensity to get a spike density function
+ if isempty(collapsed_spikes) 
+ % output array of zeros if there are no spikes
+ ff= zeros(size(testTimeVector));
+ else
+ firingDensitiy = ksdensity(collapsed_spikes,testTimeVector,'width',binSize);
+ 
+% denisty to rate 
+ ff = firingDensitiy*length(collapsed_spikes)/numTrials;
+
+ end
+ 
+ 
